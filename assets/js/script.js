b0c0a973c70295a4e27ec502c22828e21071ced9
unknown <SSDD@PNAWSA06LC1007B.duoc.acad>
2024-04-22 15:32:17 -0400

    ejemplo 1

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+
+import {robots} from './robots.js';
+console.log(robots[0]);
+
+console.log(robots[0].id);
+console.log(robots[0].name);
+console.log(robots[0].series);
+
+//const {id,name,series} = robots[0];
+//console.log(id);
+//console.log(name);
+//console.log(series);
+
+const getRobotByid =(id="009")=>{
+    return robots.find((robot)=>{
+        return robot.id ===id;
+    });
+}
+
+
+console.log(getRobotByid("013"));
+
+const getRobotBySeries =(series=2)=>{
+    return robots.filter((robot)=>{
+        return robot.series===series;
+    })
+}
+
+console.log(getRobotBySeries("3"));
+
+const {id,name,series}= getRobotByid("013");
+console.log(id);
+console.log(name);
+console.log(series);
